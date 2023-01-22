37ae189c45422dbd00dbdf151d489c899d5dc7a3
hedrikbastos <hedrikbastos@outlook.com>
2023-01-22 20:40:02 -0300

    Readme

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Slider range
 const valorSlider = document.querySelector("span");
 const inputSlider = document.querySelector("input");
 inputSlider.oninput = (()=>{
@@ -10,3 +11,30 @@ inputSlider.oninput = (()=>{
 inputSlider.onblur = (()=>{
     valorSlider.classList.remove("show");
 })
+
+//função para selecionar unico checkbox 
+function clickbox(id){
+    switch(id){
+        case muitoBom:
+            window.document.getElementById('bom').checked= false;
+            window.document.getElementById('ruim').checked= false;
+            window.document.getElementById('muitoRuim').checked= false;
+               
+            break;
+        case bom:
+            window.document.getElementById('muitoBom').checked= false;
+            window.document.getElementById('ruim').checked= false;
+            window.document.getElementById('muitoRuim').checked= false;
+            break;
+        case ruim:
+            window.document.getElementById('muitoBom').checked= false;
+            window.document.getElementById('bom').checked= false;
+            window.document.getElementById('muitoRuim').checked= false;
+            break;
+        case muitoRuim:
+            window.document.getElementById('muitoBom').checked= false;
+            window.document.getElementById('bom').checked= false;
+            window.document.getElementById('ruim').checked= false;
+            break;
+    }
+}
